b3209e67023cd704a4aceee4c6c0ea17504263f3
drug007 <drug2004@bk.ru>
2020-05-24 19:40:42 +0300

    TwoFacedRange has length

--- aux/source/aux/model.d
+++ aux/source/aux/model.d
@@ -1328,6 +1328,16 @@ private struct TwoFacedRange(Order order)
 		static if (order == Order.Bubbling) s--; else
 		static assert(0);
 	}
+
+	size_t length() const
+	{
+		static if (order == Order.Sinking)
+			return l - s;
+		else static if (order == Order.Bubbling)
+			return s;
+		else
+			static assert(0);
+	}
 }
 
 version(unittest) @Name("two_faced_range")
@@ -1340,34 +1350,44 @@ unittest
 	{
 		auto rf = TwoFacedRange!(Order.Sinking)(1, 2);
 		rf.should.be == [1];
+		rf.length.should.be == 1;
 		// lower boundary is always inclusive
 		auto rb = TwoFacedRange!(Order.Bubbling)(1, 2);
 		rb.should.be == [1, 0];
+		rb.length.should.be == 2;
 	}
 	{
 		auto rf = TwoFacedRange!(Order.Sinking)(2, 4);
 		rf.should.be == [2, 3];
+		rf.length.should.be == 2;
 		// lower boundary is always inclusive
 		auto rb = TwoFacedRange!(Order.Bubbling)(2, 4);
 		rb.should.be == [2, 1, 0];
+		rb.length.should.be == 3;
 	}
 	{
 		auto rf = TwoFacedRange!(Order.Sinking)(0, 4);
 		rf.should.be == [0, 1, 2, 3];
+		rf.length.should.be == 4;
 		auto rb = TwoFacedRange!(Order.Bubbling)(0, 4);
 		rb.should.be == [0];
+		rb.length.should.be == 1;
 	}
 	{
 		auto rf = TwoFacedRange!(Order.Sinking)(4, 4);
 		rf.should.be == empty;
+		rf.length.should.be == 0;
 		auto rb = TwoFacedRange!(Order.Bubbling)(4, 4);
 		rb.should.be == empty;
+		rb.length.should.be == 0;
 	}
 	{
 		auto rf = TwoFacedRange!(Order.Sinking)(0, 0);
 		rf.should.be == empty;
+		rf.length.should.be == 0;
 		auto rb = TwoFacedRange!(Order.Bubbling)(0, 0);
 		rb.should.be == empty;
+		rb.length.should.be == 0;
 	}
 }
 
